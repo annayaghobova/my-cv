e7828ecfd25be1263886fb5cfb03eb03eed86d8b
Anna Yaghobová <73486082+annayaghobova@users.noreply.github.com>
2026-03-13 18:08:55 +0100

    Update cv.ts

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -1,17 +1,10 @@
 export const experiences = [
 	{
-		company: 'Gymnázium Oty Pavla',
-		time: '2018-2019',
-		title: 'Teacher of CS',
+		company: 'Department of Software and Computer Science Education, Faculty of Mathematics and Physics, Charles University',
+		time: '2025-present',
+		title: 'Lector',
 		location: 'Prague, CZE',
-		description: 'I taught CS at a six-year grammar school. ',
-	},
-	{
-		company: 'Gymnázium Christiana Dopplera',
-		time: '2020-present',
-		title: 'Teacher of CS & Math',
-		location: 'Prague, CZE',
-		description: 'I am teaching CS and mathematics at an eight-year grammar school anf four-year grammar school. ',
+		description: 'I am preparing pre-service teachers of CS. ',
 	},
 	{
 		company: 'Advanced Multimedia Learning Laboratory, Faculty of Mathematics and Physics, Charles University',
@@ -21,11 +14,18 @@ export const experiences = [
 		description: '',
 	},
 	{
-		company: 'Department of Software and Computer Science Education, Faculty of Mathematics and Physics, Charles University',
-		time: '2025-present',
-		title: 'Lector',
+		company: 'Gymnázium Christiana Dopplera',
+		time: '2020-present',
+		title: 'Teacher of CS & Math',
 		location: 'Prague, CZE',
-		description: 'I am preparing pre-service teachers of CS. ',
+		description: 'I am teaching CS and mathematics at an eight-year grammar school anf four-year grammar school. ',
+	},
+	{
+		company: 'Gymnázium Oty Pavla',
+		time: '2018-2019',
+		title: 'Teacher of CS',
+		location: 'Prague, CZE',
+		description: 'I taught CS at a six-year grammar school. ',
 	},
 	// {
 	// 	company: 'Radium Institute (Institut du Radium)',
@@ -39,10 +39,10 @@ export const experiences = [
 export const education = [
 	{
 		school: 'Faculty of Mathematics and Physics, Charles University',
-		time: '2016-2019',
-		degree: 'Bachelor',
+		time: '2021-present',
+		degree: 'Ph.D.',
 		location: 'Prague, CZ',
-		description: 'Mathematics Oriented at Education - Computer Science Oriented at Education',
+		description: 'General Questions of Mathematics and Computer Science',
 	},
 	{
 		school: 'Faculty of Mathematics and Physics, Charles University',
@@ -53,10 +53,10 @@ export const education = [
 	},
 	{
 		school: 'Faculty of Mathematics and Physics, Charles University',
-		time: '2021-present',
-		degree: 'Ph.D.',
+		time: '2016-2019',
+		degree: 'Bachelor',
 		location: 'Prague, CZ',
-		description: 'General Questions of Mathematics and Computer Science',
+		description: 'Mathematics Oriented at Education - Computer Science Oriented at Education',
 	},
 	// {
 	// 	school: 'University of Paris',
@@ -88,12 +88,52 @@ export const skills = [
 
 export const publications = [
 	{
-		title: '‘The internet is in the satellites!’: A systematic review of 3–15-year-olds’ conceptions about the internet',
-		authors: 'Cyril Brom, Anna Yaghobová, Anna Drobná, Marek Urban',
-		journal: 'Education and Information Technologies',
-		time: '2023',
-		link: 'https://link.springer.com/article/10.1007/s10639-023-11775-9',
-		abstract: 'Children aged 3–15 frequently use internet-enabled technologies for leisure and educational purposes, yet they have limited knowledge about how the internet works. Literature also indicates that children possess varied and often contradictory internet-related ideas, which have not yet been systematized. This systematic review, drawing from 27 mixed-methods and qualitative studies (N = 2,214) from the last two decades (2002 – 2022), organizes these ideas through a thematic analysis with a specific focus on the age when individual child conceptions start to appear at the earliest. The findings are interpreted through the lenses of a Vygotskian framework. The results highlight 60 conceptions concerning general perceptions of the internet, internet infrastructure, and dataflow. Children appear to understand the internet primarily through their personal experiences. Perception of the internet as online activities is dominant in younger children, but also present among older ones. Children in all age groups are confused about what Wi-Fi really is; some equate it to the internet, others to the ‘Wi-Fi box’ (Wi-Fi router). The most knowledgeable older children view the internet as a global network with a complex internal, but only vaguely understood, structure. Typical reasoning among older children about the internet’s structure includes satellites, central computers or central towers. Only expert children know about distributed, server-like storage. These results imply that children at the K-8 level need to be familiarized gradually with how the internet works, but teaching them this topic may prove to be challenging. This review also highlights the limitations of existing literature and presents suggestions for future research and designing technology-based curricula.',
+		title: 'Principles of Computers - Model Lessons for Lower Secondary School: Experience Report',
+		authors: 'Cyril Brom,Anna Yaghobová, Anna Drobná, Marek Urban, Daniel Šťastný, Andrea diSessa',
+		journal: 'ISSEP 2025 Springer Proceedings',
+		time: '2025',
+		link: 'https://link.springer.com/chapter/10.1007/978-3-032-01222-7_4',
+		abstract: 'The 2021 revision of the national computing curriculum in Czechia reaffirmed the importance of teaching fundamental computing principles, including principles of computer hardware, at the ISCED level 2 (ages 11–15). Although these topics were present in the previous Czech curriculum, schools continue to face a shortage of structured, constructivist and evidence-based teaching materials that actively engage students in meaningful learning. To address this need, we developed three model lessons designed to help students grasp principles of core hardware concepts by building on their existing preconceptions. These lessons follow the constructivist Evocation – Realisation of Meaning – Reflection (ERR) framework, encouraging active exploration and reflection. The lessons were implemented and refined through design-based research in six schools across seven classes, involving approximately 160 students. Pre-post testing revealed large immediate improvements following the intervention (, Cohen’s ). This paper presents model lessons and impact of these lessons, contributing to the advancement of constructivist approaches in computing education at the lower secondary level.',
+	},
+	{
+		title: '“And What Do You Think Is inside a Computer?”: Exploring 6–9 Graders’ Preconceptions about Computer Functionality',
+		authors: 'Anna Yaghobová, Anna Drobná, Marek Urban, Cyril Brom',
+		journal: 'ACM Transactions on Computing Education',
+		time: '2025',
+		link: 'https://dl.acm.org/doi/10.1145/3769853',
+		abstract: 'Children encounter computers daily, but how do they understand what happens inside these machines? Existing literature has not examined children’s preconceptions about computer principles sufficiently. Hence, we conducted a mixed-methods study (, , ) in diverse regions of the Czech Republic with the aim of identifying the said preconceptions. We used 45-min-long semi-structured interviews and conducted an inductive thematic analysis, and identified 103 preconceptions, 90 of which have never been described in the literature previously. Based on their occurrence, we performed a two-step cluster analysis with automatic cluster detection, identifying three participant groups based on their knowledge level: experts, intermediates, and beginners. Experts comprehended the most abstract concepts, such as the interaction between RAM and the processor and the existence of different types of memory. Intermediates understood some abstract concepts, while beginner participants lacked understanding of most abstract concepts and viewed the computer as a magical black box. Our research significantly expands the number of identified preconceptions about computer principles. This could serve as a valuable basis for developing evidence-based, constructivist teaching materials and practices, addressing a gap that exists in many education systems.',
+	},
+	{
+		title: 'Learning about abstract systems: Understanding childrens journey in grasping internet principles across age groups in a mixed-methods experimental study',
+		authors: 'Cyril Brom,Anna Yaghobová, Anna Drobná, Marek Urban, Daniel Šťastný, Andrea diSessa',
+		journal: 'Computers in Human Behavior',
+		time: '2025',
+		link: 'https://doi.org/10.1016/j.chb.2025.108602',
+		abstract: '',
+	},
+	{
+		title: 'From Servers to Satellites: Understanding Internet Principles among New Computer Science Teachers',
+		authors: 'Cyril Brom, Anna Drobná, Anna Yaghobová, Daniel Št’astný, Kateřina Zábrodská, Marek Urban',
+		journal: 'ACM Transactions on Computing Education',
+		time: '2025',
+		link: 'https://dl.acm.org/doi/10.1145/3709150',
+		abstract: '',
+	},
+	{
+		title: 'Principles of the Internet - Model Lessons for Lower Secondary School: Experience Report',
+		authors: 'Anna Yaghobová, Michaela Mazná, Anna Drobná, Filip Děchtěrenko, Cyril Brom',
+		journal: 'Proceedings of the 56th ACM Technical Symposium on Computer Science Education V. 1 (SIGCSETS 2025)',
+		time: '2025',
+		link: 'https://dl.acm.org/doi/10.1145/3641554.3701977',
+		abstract: 'The national computing curriculum in Czechia was revised in 2021 to incorporate various topics, including fundamental principles of the internet and computer networks at the ISCED level 2 (age ~11-15). However, schools face a shortage of age-appropriate teaching materials on this subject. To address this gap, we developed four model lessons and corresponding materials tailored for ISCED 2 students and teachers at two levels of difficulty. The primary objective of these lessons is to elucidate the structure and fundamental principles of the internet, building on prevalent student preconceptions. This work is grounded in the constructivist instructional framework known as Evocation - Realisation of Meaning - Reflection (ERR). The lessons were created and validated through design-based research conducted across six schools, involving 13 classes and a total of approximately 270 students. Pre-post-delayed testing revealed very large immediate improvements following the intervention ( d = 1.72, p < 0.001, n = 204) as well as large, sustained effects months later ( d = 1.10, p < 0.001, n = 59). This paper details the development, implementation, and evaluation of these lessons, providing a valuable resource for educators in various contexts.',
+	},
+	{
+		title: '‘Clouds, Hacking and App Permissions: Model Lessons for Grades 5-7: Experience Report',
+		authors: 'Cyril Brom, Ondřej Petíř, Filip Dechtěrenko, Anna Drobná, Anna Yaghobová, Pavel Ježek, Kristina Volná, Kateřina Kačerovská',
+		journal: 'Proceedings of the 2024 on ACM Virtual Global Computing Education Conference V. 1 (SIGCSE Virtual 2024)',
+		time: '2024',
+		link: 'https://dl.acm.org/doi/10.1145/3649165.3690133',
+		abstract: 'While computer science education extends beyond coding, there is a dearth of evidence-based instructional materials for teaching essential non-programming concepts. This study addresses this gap by designing and evaluating three 45-minute lessons for students in Grades 5-7 on cloud storage, hacking, and smartphone app permissions. The lessons leverage the constructivist ERR framework, which promotes active learning across three phases: evocation (activating prior knowledge), realization of meaning (presenting new information), and reflection (solidifying under-standing). Pre- and post-tests (N = 82) demonstrated significant learning gains for all three lessons (d = 0.69 - 1.49), showing the effectiveness of the lessons and, more broadly, the ERR framework applied in computer science education in Grades 5-7. The paper details the lessons such that they can be reused in other contexts and introduces an evaluation of the lessons, presenting valuable insights for both educators and researchers interested in developing effective computer science education resources.',
 	},
 	{
 		title: '‘The internet is in the satellites!’: A systematic review of 3–15-year-olds’ conceptions about the internet',
@@ -111,4 +151,66 @@ export const publications = [
 	// 	link: '#',
 	// 	abstract: 'Early research leading to the identification of uranium’s radioactive properties.',
 	// },
+];
+
+export const grants = [
+	{
+		title: 'AI assistants for teaching computer science on lower secondary school level.',
+		time: '2025 - 2027',
+		organization: 'TAČR TQ23000146, Technology Agency of the Czech Republic',
+		location: 'Principal Investigator in partner organization',
+		description: 'partnering with AI dětem',
+	},
+	{
+		title: 'Development of instructional materials for lower-secondary education in digital technologies.',
+		time: '2022 - 2025',
+		organization: 'GAUK 360322, Charles University Grant Agency',
+		location: 'Principal Investigator',
+		description: '',
+	},
+	{
+		title: 'Pregraduate preparation of future primary school teachers for teaching digital technologies in the light of current curriculum changes.',
+		time: '2022 - 2024',
+		organization: 'GAUK 484722, Charles University Grant Agency',
+		location: 'Researcher',
+		description: '',
+	},
+	{
+		title: 'Internet4Kids - Building Internet knowledge: a theory of conceptual change perspective.',
+		time: '2022 - 2024',
+		organization: 'GAČR 22-20771S, Czech Science Foundation',
+		location: 'Researcher',
+		description: '',
+	},
+];
+
+export const researchStays = [
+	{
+		title: 'Forschungsprofessur Informatikdidaktik',
+		time: 'October 2025',
+		organization: 'Pädagogische Hochschule Schwyz',
+		location: 'Schwyz, SUI',
+		description: 'Academic internship, supervisor Prof. Dr. Eva Marinus.',
+	},
+	{
+		title: 'Abteilung Didaktik der Fakultät für Mathematik und Informatik',
+		time: 'September 2024',
+		organization: 'Friedrich-Schiller-Universität Jena',
+		location: 'Jena, DEU',
+		description: 'Academic internship, supervisor Prof. Dr. Michael Rücker.',
+	},
+	{
+		title: 'EERA Summer School 2024',
+		time: 'July 2024',
+		organization: 'University of Porto',
+		location: 'Porto, PRT',
+		description: '',
+	},
+	{
+		title: 'Institut für Mathematik und Angewandte Informatik',
+		time: 'April 2024',
+		organization: 'Stiftung Universität Hildesheim',
+		location: 'Hildesheim, DEU',
+		description: 'Academic internship, supervisor Prof. Dr. Nils Pancratz.',
+	},
 ];
